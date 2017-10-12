68233134f6f2791b8361681f531936f533c29b3f
Tonko Mulder <t.mulder@jvdmedia.nl>
2017-10-12 23:49:57 +0200

    added container for Vue

---
+++ resources/views/backoffice/index.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Co-housing blade and Vue.js</title>
+        <link rel="stylesheet" href="/css/app.css">
+    </head>
+    <body>
+        <div id="app"></div>
+        <script src="/js/app.js"></script>
+    </body>
+</html>
