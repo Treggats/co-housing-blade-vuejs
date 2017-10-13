fef6755095031908834dc015060e80299e90b289
Tonko Mulder <Treggats@users.noreply.github.com>
2017-10-13 22:58:56 +0200

    final version, merge into master (#1)
    * initial commit
    
    * added container for Vue
    
    * added PageController & BackofficePageController
    
    * updated route to point to the pagecontroller
    
    * moved BackofficePageController to it's own namespace
    
    * added route for backoffice
    
    * added example component to root div

---
+++ resources/views/backoffice/index.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Co-housing blade and Vue.js</title>
+        <link rel="stylesheet" href="/css/app.css">
+    </head>
+    <body>
+        <div id="app">
+            <example></example>
+        </div>
+        <script src="/js/app.js"></script>
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Routing\Router;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'PageController@index');
+
+Route::group(['prefix' => 'backoffice'], function (Router $router) {
+    $router->get('/', 'Backoffice\PageController@index');
+});
