605c14393bb3d149bc70a6f813a961f602c18c0a
Tonko Mulder <t.mulder@jvdmedia.nl>
2017-10-13 22:50:58 +0200

    moved BackofficePageController to it's own namespace

--- app/Http/Controllers/Backoffice/PageController.php
+++ app/Http/Controllers/Backoffice/PageController.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Backoffice;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
-class BackofficePageController extends Controller
+class PageController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -13,7 +14,7 @@ class BackofficePageController extends Controller
      */
     public function index()
     {
-        //
+        return view('backoffice.index');
     }
 
     /**
